20a65fa312e0845d1a4b81760a2b27145804f0b4
jerebacher <bacherjere@gmail.com>
2017-11-12 17:38:35 -0300

    Con modificaciones

--- archivos/Cola.py
+++ archivos/Cola.py
@@ -2,17 +2,20 @@ class Cola:
 	"""Representa a una cola, con operaciones de encolar, desencolar, ver si esta vacia o no y ver primero. El primero en ser encolado es también el primero en ser desencolado."""
 	def __init__(self):
 		"""Crea una cola vacía."""
-		self.items = []
+		self.items = [0, ]
+		self.pos_actual = 0
 
 	def encolar(self, x):
 		"""Encola el elemento x."""
 		self.items.append(x)
+		self.pos_actual += 1
 
 	def desencolar(self):
 		"""Elimina el primer elemento de la cola y devuelve su valor. Si la cola está vacía, levanta ValueError."""
 		if self.esta_vacia():
 			raise ValueError("La cola está vacía")
 		return self.items.pop(0)
+		self.pos_actual -= 1
 
 	def esta_vacia(self):
 		"""Devuelve True si la cola esta vacía, False si no."""
@@ -23,3 +26,7 @@ class Cola:
 		if not self.esta_vacia:
 			return "La pila esta vacia."
 		return self.items[0]
+
+	def ver_pos_actual(self):
+		"""..."""
+		return self.pos_actual
