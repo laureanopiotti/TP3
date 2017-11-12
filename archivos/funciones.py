d7af7bafd8ebb82b645b32cfb8b722a4486ab097
jerebacher <bacherjere@gmail.com>
2017-11-12 17:13:33 -0300

    Modificaciones en el archivo actual

--- archivos/funciones.py
+++ archivos/funciones.py
@@ -1,24 +1,18 @@
+from Pila import Pila
+from Cola import Cola
 #Funciones
 
-
 def validar(archivo):
-	"""Recibe un archivo de tipo sceql y lo recorre caracter por caracter filtrando por los comandos validos del lenguaje 
-	SCEQL"""
-
+	"""Recibe un archivo de tipo sceql y lo recorre caracter por caracter filtrando por los comandos validos del lenguaje SCEQL"""
 	try:
-                with open(archivo) as archivo_a_recorrer:
-        except IOError('Problema con el archivo')
-
-        linea_a_devolver = ""
-        sceql_comandos = ['!','=','-','_','/','\\']
-        for linea in archivo_a_recorrer:
-                linea = linea.rstrip('/n').replace(' ','')
-                for char in linea:
-                        if char in sceql_comandos:
-                                linea_a_devolver += char
-
-        
-                        
-                
-                
- 		
+		with open(archivo) as archivo_a_recorrer:
+			linea_a_devolver = ""
+			sceql_comandos = dict.fromkeys(['!','=','-','_','/','\\'])
+			for linea in archivo_a_recorrer:
+				linea = linea.rstrip('/n').replace(' ','')
+				for char in linea:
+					if char in sceql_comandos:
+						linea_a_devolver += char
+			print(linea_a_devolver)
+	except IOError:
+		return 'Problema con el archivo'
