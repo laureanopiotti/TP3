268f419c4fc88a15cb6b1ec58e188ffa512e0f98
laureanopiotti <33470643+laureanopiotti@users.noreply.github.com>
2017-11-12 16:52:46 -0300

    recorrer por lineas, limpiar
    sacar espacios, obtener caracteres sceql

--- archivos/funciones.py
+++ archivos/funciones.py
@@ -1,1 +1,24 @@
 #Funciones
+
+
+def validar(archivo):
+	"""Recibe un archivo de tipo sceql y lo recorre caracter por caracter filtrando por los comandos validos del lenguaje 
+	SCEQL"""
+
+	try:
+                with open(archivo) as archivo_a_recorrer:
+        except IOError('Problema con el archivo')
+
+        linea_a_devolver = ""
+        sceql_comandos = ['!','=','-','_','/','\\']
+        for linea in archivo_a_recorrer:
+                linea = linea.rstrip('/n').replace(' ','')
+                for char in linea:
+                        if char in sceql_comandos>
+                                linea_a_devolver += char
+
+        
+                        
+                
+                
+ 		
